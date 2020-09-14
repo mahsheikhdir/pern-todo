86b555411d60e93607cb8eac3394c45bba543ae2
Mahdir Sheikh <mahsheikhdir@gmail.com>
2020-09-14 11:48:05 -0700

    added client

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+
+//components
+
+import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodos";
+
+
+function App() {
+  return (<Fragment>
+    <div className="container">
+      <InputTodo/>
+      <ListTodo/>
+    </div></Fragment>
+  );
+}
+
+export default App;
